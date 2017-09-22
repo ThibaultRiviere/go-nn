f54134104b21533fcc162fe46b103025bdbfcc06
Thibault Riviere <thibault.riviere@scality.com>
2017-09-22 13:11:32 +0200

    ft(tree): reorder sources

--- pkgs/nn/activation.go
+++ pkgs/nn/activation.go
@@ -8,5 +8,5 @@ func Dsigmoid(sum float64) float64 { return sum * (1 - sum) }
 func tanh(sum float64) float64  { return math.Tanh(sum) }
 func Dtanh(sum float64) float64 { return 1.0 - sum*sum }
 
-func activation(sum float64) float64           { return tanh(sum) }
-func activationDerivative(sum float64) float64 { return Dtanh(sum) }
+func activation(sum float64) float64           { return sigmoid(sum) }
+func activationDerivative(sum float64) float64 { return Dsigmoid(sum) }
